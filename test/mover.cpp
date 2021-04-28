c14f7472397e8e612497b52bc74749ecf4748e54
Jeff DeWall <sr.jilarious@fastmail.net>
2021-04-27 19:37:13 -0600

    Changed mover to do what it initially was meant to do.

--- test/mover.cpp
+++ test/mover.cpp
@@ -10,55 +10,57 @@ int main(int argc, char** argv)
 {
     termable::termableLinux term;
     term.clear();
+    term.showCursor(false);
+
+    auto sz = term.displaySize();    
+    termable::termBuffer buffer1(sz), buffer2(sz);
+    buffer1.fill({' '}, termable::color::basic::BoldWhite, termable::color::basic::Black);
+    buffer2.fill({'.'}, termable::color::basic::BoldWhite, termable::color::basic::Black);
+
+    termable::termBuffer *currBuffer = &buffer1;
+    termable::termBuffer *oldBuffer = &buffer2;
+    termable::vec2i pos = {sz.x/2,sz.y/2};
     termable::KeyResult c = std::monostate{};
     while(true) {
         c = term.getch();
         if(std::holds_alternative<char>(c)) {
             auto ch = std::get<char>(c);
-            printf("%d, ", ch);
-            fflush(stdout);
-
             if(ch == 'q') {
                 break;
             }
         }
         else if(std::holds_alternative<termable::NonAsciiChar>(c)) {
             auto nac = std::get<termable::NonAsciiChar>(c);
+
             switch(nac) {
                 case termable::NonAsciiChar::Up:
-                    printf("!Up! ");
+                    pos.y--;
+                    if(pos.y <= 0) pos.y = 0;
                     break;
                 case termable::NonAsciiChar::Left:
-                    printf("!Left! ");
+                    pos.x--;
+                    if(pos.x <= 0) pos.x = 0;
                     break;
                 case termable::NonAsciiChar::Down:
-                    printf("!Down! ");
+                    pos.y++;
+                    if(pos.y >= sz.y-1) pos.y = sz.y-1;
                     break;
                 case termable::NonAsciiChar::Right:
-                    printf("!Right! ");
-                    break;
-                case termable::NonAsciiChar::Home:
-                    printf("!Home! ");
-                    break;
-                case termable::NonAsciiChar::End:
-                    printf("!End! ");
-                    break;
-                case termable::NonAsciiChar::PageUp:
-                    printf("!PageUp! ");
-                    break;
-                case termable::NonAsciiChar::PageDown:
-                    printf("!PageDown! ");
-                    break;
-                case termable::NonAsciiChar::Delete:
-                    printf("!Delete! ");
-                    break;
-                case termable::NonAsciiChar::Insert:
-                    printf("!Insert! ");
+                    pos.x++;
+                    if(pos.x >= sz.x-1) pos.x = sz.x-1;
                     break;
             }
+            
+            currBuffer->fill({' '});
+            currBuffer->writeChar(pos, {'*'});
+            term.renderBuffer(*currBuffer, *oldBuffer);
+            // Swap the buffers.
+            std::swap(currBuffer, oldBuffer);
+        
             fflush(stdout);
         }
 
     }
+    term.showCursor(false);
     return 0;
 }
